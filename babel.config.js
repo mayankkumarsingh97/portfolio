f060d5fbdb6502fd4d5d55d5656957e3fd486677
Mayank kumar singh <mayank.reactjs@gmail.com>
2025-09-01 17:14:34 +0530

    plugins added based on env type

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,8 @@
+// module.exports = {
+//   presets: ["@babel/preset-env", "@babel/preset-react"]
+// };
+// babel.config.js
 module.exports = {
-  presets: ["@babel/preset-env", "@babel/preset-react"]
+  presets: ['@babel/preset-env', '@babel/preset-react'],
+  plugins: process.env.NODE_ENV === 'development' ? ['react-refresh/babel'] : [],
 };
