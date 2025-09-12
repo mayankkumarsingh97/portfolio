5e378fb168d366f7e5eab4a41807845910faf746
Mayank <66870313+mayankkumarsingh97@users.noreply.github.com>
2025-09-12 20:59:41 +0530

    Update babel.config.js
    Comments added in babel.config.js file

--- babel.config.js
+++ babel.config.js
@@ -6,3 +6,7 @@ module.exports = {
   presets: ['@babel/preset-env', '@babel/preset-react'],
   plugins: process.env.NODE_ENV === 'development' ? ['react-refresh/babel'] : [],
 };
+//
+//I have made some changes on remote of feat/dev-user-auth branch 
+//
+// In babel file
