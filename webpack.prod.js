3fb1ad060147fd8030e487c42bb025ce49ec4da4
Mayank kumar singh <mayank.reactjs@gmail.com>
2025-09-12 23:53:37 +0530

    add: all optimization plugins;CompressionPlugin;CopyPlugin;PurgeCSSPlugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -8,42 +8,43 @@ const TerserPlugin = require("terser-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const CompressionPlugin = require("compression-webpack-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-//
-const commonConfig = require("./webpack.common");
+const safeParser = require("postcss-safe-parser");
 const { merge } = require("webpack-merge");
-//
+
+const commonConfig = require("./webpack.common");
+
 const PATHS = {
   src: path.join(__dirname, "src"),
 };
 
 module.exports = merge(commonConfig, {
   mode: "production",
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-      {
-        test: /\.ts$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        type: "asset/resource",
-        generator: {
-          filename: "images/[name].[contenthash][ext]",
-        },
-      },
-    ],
-  },
+
   plugins: [
-    new MiniCssExtractPlugin(),
-    new PurgeCSSPlugin({
-      paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true }),
+    // Extract CSS
+    new MiniCssExtractPlugin({
+      filename: "assets/css/[name].[contenthash].css",
     }),
+
+    // Purge unused CSS
+    new PurgeCSSPlugin({
+      paths: glob.sync(`${PATHS.src}/**/*.{js,jsx,ts,tsx,html}`, {
+        nodir: true,
+      }),
+
+      // Ensure it only scans templates, not CSS
+      only: [() => "main"],
+
+      safelist: {
+        standard: [/^swiper-/, /^slick-/, /^fa-/, /^mdi-/], // keep 3rd-party libs
+        deep: [/^chakra-/, /^ant-/], // nested class patterns if using UI libs
+      },
+    }),
+
+    // Load env variables
     new Dotenv(),
+
+    // Copy only required static assets
     new CopyPlugin({
       patterns: [
         {
@@ -52,13 +53,27 @@ module.exports = merge(commonConfig, {
         },
       ],
     }),
-    new CompressionPlugin(),
+
+    // Pre-compression (gzip + brotli)
+    new CompressionPlugin({
+      algorithm: "brotliCompress",
+      filename: "[path][base].br",
+      test: /\.(js|css|html|svg|woff2?)$/,
+      compressionOptions: { level: 11 },
+      deleteOriginalAssets: false,
+    }),
   ],
+
   optimization: {
     minimize: true,
     minimizer: [
+      // Minify CSS
       new CssMinimizerPlugin(),
+
+      // Minify JS
       new TerserPlugin(),
+
+      // Optimize Images
       new ImageMinimizerPlugin({
         minimizer: {
           implementation: ImageMinimizerPlugin.imageminMinify,
@@ -66,11 +81,26 @@ module.exports = merge(commonConfig, {
             plugins: [
               ["mozjpeg", { quality: 60 }],
               ["pngquant", { quality: [0.4, 0.5] }],
+              ["svgo", { plugins: [{ removeViewBox: false }] }],
             ],
           },
         },
       }),
     ],
+
+    // Enable tree-shaking + module pruning
     usedExports: true,
+
+    // Split vendor & runtime bundles
+    splitChunks: {
+      chunks: "all",
+    },
+    runtimeChunk: "single",
+  },
+
+  performance: {
+    hints: "warning",
+    maxEntrypointSize: 512000, // 500 KB
+    maxAssetSize: 512000,
   },
 });
